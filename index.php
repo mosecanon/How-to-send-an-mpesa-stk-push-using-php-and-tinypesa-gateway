bd57591759a51b23fcd6487f2c81616911f2632c
Mose Canon <58547280+mosecanon@users.noreply.github.com>
2023-09-29 12:53:10 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+$phonenumber = '0741575274'; // Phone number paying
+$Account_no = '01020223246000'; // Enter account number optional
+$amount = 1;
+$url = 'https://tinypesa.com/api/v1/express/initialize';
+
+$data = array(
+    'amount' => $amount,
+    'msisdn' => $phonenumber,
+    'account_no'=> $Account_no
+);
+$headers = array(
+    'Content-Type: application/x-www-form-urlencoded',
+    'ApiKey: UPLexi196J6' // Replace with your api key
+ );
+
+$info = http_build_query($data);
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $info);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+$resp = curl_exec($curl);
+$msg_resp = json_decode($resp);
+print_r($msg_resp);
+?>
